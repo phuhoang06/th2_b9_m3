628536c16e479fb9178d408d0582cc910ec4093e
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-12-24 13:10:55 +0700

    add

--- src/main/java/com/example/th2/HelloServlet.java
+++ src/main/java/com/example/th2/HelloServlet.java
@@ -1,27 +1,27 @@
 package com.example.th2;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 
-@WebServlet(name = "LoginServlet", value = "/login")
-public class LoginServlet extends HttpServlet {
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
 
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        if ("admin".equals(username) && "admin".equals(password)) {
-            writer.println("<h1>Welcome " + username + " to website</h1>");
-        } else {
-            writer.println("<h1>Login Error</h1>");
-        }
-        writer.println("</html>");
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
     }
 }
